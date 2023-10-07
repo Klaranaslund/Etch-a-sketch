7c0a12fd779d043674a1155cd46cd39978e3a4f4
Klaranaslund <naslundklara@gmail.com>
2023-10-07 10:10:44 +0200

    first functional draft where drawing, erasing and clearing the grid can be performed by clicking corresponding buttons

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
-/**Create a 16x16 grid of square divs using JavaScript, to be 
- * kept inside a container div which goes directly into html-file.
- */
-
 const container = document.querySelector('.GridContainer');
+let mode = '';
+
+let blackbutton = document.querySelector('#black');
+let erasebutton = document.querySelector('#erase');
+let clearbutton = document.querySelector('#clear');
+
+blackbutton.addEventListener('click', () => setDrawingMode('black'));
+erasebutton.addEventListener('click', () => setDrawingMode('erase'));
+clearbutton.addEventListener('click', () => clearDivs());
+
+function setDrawingMode(newMode){
+    mode = newMode;
+}
 
 /*Calculate percent to set correct flex-value*/
 function getGridPercent(gridsize){
@@ -23,55 +32,27 @@ function addGrid(gridsize){
     for (let i = 0; i <(gridsize*gridsize); i++){
         let gridDiv = document.createElement('div');
         setGridStyle(gridDiv, gridsize);
+        gridDiv.addEventListener('mouseover', () => drawOnGrid(gridDiv));
         container.appendChild(gridDiv);
     }
 }
 
-function getGridSize(){
-    /** Get gridsize from user
-     * return gridzise
-     */
+addGrid(16); 
+
+function clearDivs() {
+    document.querySelectorAll('.gridDiv').forEach(gridDiv => {
+        gridDiv.style.backgroundColor = 'white';
+    });
 }
 
-function getDrawingMode(){
-    /** Get mode from user (black, rainbow, erase, clear)
-     * return mode
-     */
+function drawOnGrid(gridDiv){
+    switch(mode){
+        case 'black': 
+            gridDiv.style.backgroundColor = 'black';
+            break;
+        case 'erase': 
+            gridDiv.style.backgroundColor = 'white';
+            break;
+    }
 }
-
-
-function drawOnGrid(){
-    /** change classes for divs that are hovered to give illusion of coloring
-     * if-statement or switch case to determine if they should be black, colored, white (erased), clear
-     */
-}
-
-
-
-addGrid(4); 
-
-
-
-/**Set up hover effect so that the grid disv change color when your mouse 
- * passes over them, leaving a trail trough the grid. 
- * 
- * Hovering is what happens when mous enters a div and ends when it leaves, 
- * can be set up with event listeners for either of those events as starting points.
- * 
- * change color by: adding a new class to the div or changing the
- * divs background color using javascript.
- */
-
-
-/** Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. 
- * Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) 
- * so that you’ve got a new sketch pad. 
- * 
- * Tip: Set the limit for the user input to a maximum of 100.
- * A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
- * 
- * 
- * 
- * Research button tags in HTML and how you can make a JavaScript function run when one is clicked. Also check out prompts.
- * You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used
- */
+  
