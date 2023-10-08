d7407bdc797dda3b5c4e933d1081ec252377357f
Klaranaslund <naslundklara@gmail.com>
2023-10-08 15:47:27 +0200

    Add styling to buttons and size up gridcontainer in style.css. Add restriction to input size of grid in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+/** TO DO:
+ * Se till att divs inte ritas på förrän mode är valt igen efter clear.
+ * 
+ * Lägg till kommentarer
+ */
+
+
 const container = document.querySelector('.GridContainer');
 let mode = '';
 let size = 16;
@@ -25,19 +32,25 @@ function getGridPercent(gridsize){
 
 function setSize(){
     let newSize = prompt('Type size of grid!');
+    while (newSize <1 || newSize >100){
+        newSize = prompt('Invalid size, must be larger than 1 and smaller than 100. Type size of grid!');
+    }
     removeGrid();
     addGrid(newSize)
 }
 
-/**Set divs in grid to class gridDiv, set flex-basis(?) depending on the size of the grid */
+/**Set divs in grid to class gridDiv, set flex-basis depending on the size of the grid */
 function setGridStyle(grid, gridsize) {
     let gridprcnt = getGridPercent(gridsize);
     grid.setAttribute('class', 'gridDiv');
-    grid.setAttribute('style', `flex: 0 0 calc(${gridprcnt}%)`);
+    grid.setAttribute('style', `flex: 0 0 calc(${gridprcnt}% - 2px); border: 1px solid black;`);
 }
 
 
-/** Should add a grid of divs, 16x16. */
+
+/** Adds a grid of divs to container, size depending on input from user. 
+ *  Adds eventlistener and sets the styling for all divs in the grid.
+ * */
 function addGrid(gridsize){
     for (let i = 0; i <(gridsize*gridsize); i++){
         let gridDiv = document.createElement('div');
@@ -47,18 +60,28 @@ function addGrid(gridsize){
     }
 }
 
+/**
+ * Removes all divs currently making up the grid.
+ */ 
 function removeGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
 
+/**
+ * Colors all divs white.
+ */
 function clearGrid() {
     document.querySelectorAll('.gridDiv').forEach(gridDiv => {
         gridDiv.style.backgroundColor = 'white';
     });
 }
 
+/**
+ * Allows user to 'draw' on grid with either black or white (erasing), depending on the 
+ * current mode set by the user.
+ */
 function drawOnGrid(gridDiv){
     switch(mode){
         case 'black': 
